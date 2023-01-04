84429c96b8c9f0263874a9d9f09c03b2ab2d7f70
Joshua Manchester <joshuamanchester4@gmail.com>
2023-01-04 12:34:32 +0000

    Fix omnisharp semantic tokens

--- lua/plugins/lsp/servers.lua
+++ lua/plugins/lsp/servers.lua
@@ -128,6 +128,44 @@ M.servers = function(on_attach, capabilities)
                     on_attach(client, bufnr)
                 end,
                 capabilities = capabilities,
+                -- HACK: Need to overwrite on_new_config and clear the new_config.cmd table to fix LSP restarts duplication cmd args
+                on_new_config = function(new_config, new_root_dir)
+                    new_config.cmd = { new_config.cmd[1] }
+                    table.insert(new_config.cmd, "-z") -- https://github.com/OmniSharp/omnisharp-vscode/pull/4300
+                    vim.list_extend(new_config.cmd, { "-s", new_root_dir })
+                    vim.list_extend(new_config.cmd, { "--hostPID", tostring(vim.fn.getpid()) })
+                    table.insert(new_config.cmd, "DotNet:enablePackageRestore=false")
+                    vim.list_extend(new_config.cmd, { "--encoding", "utf-8" })
+                    table.insert(new_config.cmd, "--languageserver")
+
+                    if new_config.enable_editorconfig_support then
+                        table.insert(new_config.cmd, "FormattingOptions:EnableEditorConfigSupport=true")
+                    end
+
+                    if new_config.organize_imports_on_format then
+                        table.insert(new_config.cmd, "FormattingOptions:OrganizeImports=true")
+                    end
+
+                    if new_config.enable_ms_build_load_projects_on_demand then
+                        table.insert(new_config.cmd, "MsBuild:LoadProjectsOnDemand=true")
+                    end
+
+                    if new_config.enable_roslyn_analyzers then
+                        table.insert(new_config.cmd, "RoslynExtensionsOptions:EnableAnalyzersSupport=true")
+                    end
+
+                    if new_config.enable_import_completion then
+                        table.insert(new_config.cmd, "RoslynExtensionsOptions:EnableImportCompletion=true")
+                    end
+
+                    if new_config.sdk_include_prereleases then
+                        table.insert(new_config.cmd, "Sdk:IncludePrereleases=true")
+                    end
+
+                    if new_config.analyze_open_documents_only then
+                        table.insert(new_config.cmd, "RoslynExtensionsOptions:AnalyzeOpenDocumentsOnly=true")
+                    end
+                end,
             })
         end,
     }

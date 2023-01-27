54dc2f766cdae174c035847e400f156a5dd994d3
Joshua Manchester <joshuamanchester4@gmail.com>
2023-01-27 10:02:53 +0000

    Check if formatter is installed

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -3,25 +3,27 @@ return {
     dependencies = "nvim-lua/plenary.nvim",
     event = "BufReadPre",
     config = function()
+        local sources = {
+            require("null-ls").builtins.formatting.black,
+            require("null-ls").builtins.code_actions.gitsigns,
+            require("null-ls").builtins.formatting.rustfmt,
+            require("null-ls").builtins.formatting.csharpier,
+            require("null-ls").builtins.formatting.prettier.with({
+                filetypes = { "html", "javascript" },
+                extra_args = { "--tab-width", 4 }
+            }),
+            -- require("null-ls").builtins.code_actions.eslint_d,
+            -- require("null-ls").builtins.diagnostics.eslint_d,
+        }
+
+        if vim.fn.executable('clang-format') then
+            table.insert(sources, require("null-ls").builtins.formatting.clang_format.with({
+                extra_args = { "-style={IndentWidth: 4}" },
+            }))
+        end
+
         require("null-ls").setup({
-            sources = {
-                -- require("null-ls").builtins.formatting.stylua.with({
-                --     extra_args = { "--indent-type", "Spaces" },
-                -- }),
-                require("null-ls").builtins.formatting.black,
-                require("null-ls").builtins.code_actions.gitsigns,
-                require("null-ls").builtins.formatting.rustfmt,
-                require("null-ls").builtins.formatting.clang_format.with({
-                    extra_args = { "-style={IndentWidth: 4}" },
-                }),
-                require("null-ls").builtins.formatting.csharpier,
-                require("null-ls").builtins.formatting.prettier.with({
-                    filetypes = { "html", "javascript" },
-                    extra_args = { "--tab-width", 4 }
-                }),
-                -- require("null-ls").builtins.code_actions.eslint_d,
-                -- require("null-ls").builtins.diagnostics.eslint_d,
-            },
+            sources = sources,
             on_attach = require("plugins.lsp.formatting").on_attach
         })
     end,

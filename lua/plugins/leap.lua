e18dd349367a4f6df74e35f2dc3e86846391e7db
Joshua Manchester <joshuamanchester4@gmail.com>
2023-01-27 10:03:42 +0000

    Remove flit plugin

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -8,12 +8,6 @@ return {
                 paste_on_remote_yank = true,
             },
         },
-        {
-            "ggandor/flit.nvim",
-            opts = {
-                labeled_modes = "nv",
-            },
-        },
     },
     config = function()
         require("leap").add_default_mappings()

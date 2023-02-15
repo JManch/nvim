0e3030b55326b60178be8f38849472e7c1032c58
Joshua Manchester <joshuamanchester4@gmail.com>
2023-02-15 22:05:16 +0000

    Use vim.o instead of vim.opt where possible

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,53 +1,58 @@
 local g = vim.g
+local o = vim.o
+local opt = vim.opt
 local api = vim.api
 
 -- General
-vim.opt.mouse = 'a' -- Enable mouse support
-vim.opt.exrc = true -- Load vim config in current dir
-vim.opt.ignorecase = true -- Case-insensitive search
-vim.opt.smartcase = true -- Smart case-sensitive search
-vim.opt.swapfile = true -- Enable swap files
-vim.opt.undofile = true -- Store undo history
+o.mouse = 'a' -- Enable mouse support
+o.exrc = true -- Load vim config in current dir
+o.ignorecase = true -- Case-insensitive search
+o.smartcase = true -- Smart case-sensitive search
+o.swapfile = true -- Enable swap files
+o.undofile = true -- Store undo history
+opt.jumpoptions = { 'stack' } -- Test this
 if vim.version().minor > 8 then
-  vim.opt.diffopt:append('linematch:60')
+  opt.diffopt:append('linematch:60')
 end
 g.mapleader = ' ' -- Custom leader
 g.maplocalleader = ',' -- Local leader used for neorg
 api.nvim_del_keymap('n', '<C-L>') -- Delete clashing keymap
 
 -- UI
-vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
-vim.opt.relativenumber = true -- Show relative line numbers
-vim.opt.number = true -- Show absolute line number
-vim.opt.wrap = false -- Disable text wrapping
-vim.opt.linebreak = true -- Wrap at words rather than characters
-vim.opt.termguicolors = true -- Enable 24-bit RBG colors
-vim.opt.scrolloff = 8 -- Scroll before reaching edge
-vim.opt.signcolumn = 'yes' -- Diagnostic icon column
-vim.opt.showmode = false -- Hide mode
-vim.opt.splitright = true -- Vertical split to the right
-vim.opt.splitbelow = true -- Horizontal split below
-vim.opt.showtabline = 0 -- Hide tabline
-vim.opt.list = true -- Show tab, space and new line chars
-vim.opt.listchars:append('trail:·') -- Custom space char
-vim.opt.listchars:append('tab:  ') -- Custom tab char
-vim.opt.cmdheight = 0 -- Hide command bar
-vim.opt.fillchars = { eob = ' ' }
-vim.opt.laststatus = 3 -- Global statusline
--- vim.opt.winbar = '%=%m %t' -- Custom winbar
+o.relativenumber = true -- Show relative line numbers ok this is some super loing tex asd asd asd asd asd asd kjasd asdkjas dkasj aksdj aksdj askd askd asdka sdaksdjaks asd kjasd aksd aksd jaskdja skdas asdasdasd
+o.number = true -- Show absolute line number
+o.wrap = false -- Disable text wrapping
+o.linebreak = true -- Wrap at words rather than characters
+o.termguicolors = true -- Enable 24-bit RBG colors
+o.scrolloff = 8 -- Scroll before reaching edge
+o.signcolumn = 'yes' -- Diagnostic icon column
+o.showmode = false -- Hide mode
+o.splitright = true -- Vertical split to the right
+o.splitbelow = true -- Horizontal split below
+o.showtabline = 0 -- Hide tabline
+o.list = true -- Show tab, space and new line chars
+o.cmdheight = 0 -- Hide command bar
+o.laststatus = 3 -- Global statusline
+o.emoji = false
+opt.completeopt = { 'menu', 'menuone', 'noselect' }
+opt.fillchars = { eob = ' ' }
+opt.listchars:append({
+  trail = '·',
+  tab = '  ',
+})
 
 -- Tabs, indent and folding
-vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.shiftwidth = 4 -- Shift 4 spaces when tab
-vim.opt.tabstop = 4 -- 1 tab == 4 spaces
-vim.opt.smartindent = true -- Autoindent new lines
-vim.opt.foldenable = false -- Disable folding
-vim.opt.textwidth = 79 -- 79 is default anyway
+o.expandtab = true -- Use spaces instead of tabs
+o.shiftwidth = 4 -- Shift 4 spaces when tab
+o.tabstop = 4 -- 1 tab == 4 spaces
+o.smartindent = true -- Autoindent new lines
+o.foldenable = false -- Disable folding
+o.textwidth = 80 -- For comment autowrap
 
 -- Performance
-vim.opt.hidden = true -- Enable background buffers
-vim.opt.updatetime = 250 -- Trigger event wait time (ms)
-vim.opt.timeoutlen = 600 -- Time to wait for mapped sequence (ms)
+o.hidden = true -- Enable background buffers
+o.updatetime = 250 -- Trigger event wait time (ms)
+o.timeoutlen = 600 -- Time to wait for mapped sequence (ms)
 
 -- Disable providers
 g['loaded_python3_provider'] = 0

5da275b23bfbee9beefb09028e1a3300000764b6
Joshua Manchester <joshuamanchester4@gmail.com>
2023-02-17 18:30:44 +0000

    Increase luasnip source keyword length

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -62,7 +62,7 @@ M.config = function()
     },
     sources = cmp.config.sources({
       { name = 'neorg', keyword_length = 1 },
-      { name = 'luasnip', keyword_length = 2 },
+      { name = 'luasnip', keyword_length = 4 },
       { name = 'nvim_lsp', keyword_length = 1 },
       { name = 'buffer', keyword_length = 3 },
       { name = 'path' },

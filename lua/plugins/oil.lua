137eff7e3977dc2c3407a647352f80905c21e93b
Joshua Manchester <joshuamanchester4@gmail.com>
2023-02-25 01:19:14 +0000

    Add oil plugin

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,42 @@
+return {
+  'stevearc/oil.nvim',
+  cmd = 'OilFloat',
+  keys = {
+    { '<LEADER>fd', '<CMD>OilFloat<CR>' },
+  },
+  opts = {
+    columns = {
+      'icon',
+      'mtime',
+    },
+    win_options = {
+      number = true,
+      relativenumber = true,
+      concealcursor = 'nv',
+    },
+    keymaps = {
+      ['<C-v>'] = 'actions.select_vsplit',
+      ['<C-x>'] = 'actions.select_split',
+      ['gg'] = 'actions.parent',
+      ['gh'] = 'actions.toggle_hidden',
+      ['q'] = 'actions.close',
+    },
+    float = {
+      padding = 5,
+      max_width = 120,
+      win_options = {
+        winblend = 0,
+      },
+    },
+  },
+  config = function(_, opts)
+    vim.api.nvim_create_user_command('OilFloat', function(args)
+      if #args.fargs == 0 then
+        require('oil').open_float()
+      else
+        require('oil').open_float(args.fargs[1])
+      end
+    end, { nargs = '*', complete = 'dir' })
+    require('oil').setup(opts)
+  end,
+}

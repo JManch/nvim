a05ebd4224fb95f5fc06a5f87759f50c847338e5
Joshua Manchester <joshuamanchester4@gmail.com>
2023-04-17 17:53:45 +0100

    Remove slow vim.fn.executable checks

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -7,30 +7,15 @@ return {
       require('null-ls').builtins.formatting.stylua,
       require('null-ls').builtins.formatting.rustfmt,
       require('null-ls').builtins.formatting.csharpier,
+      require('null-ls').builtins.formatting.clang_format.with({
+        extra_args = { '-style={IndentWidth: 4}' },
+      }),
+      require('null-ls').builtins.formatting.black,
+      require('null-ls').builtins.formatting.prettier.with({
+        filetypes = { 'html', 'javascript' },
+        extra_args = { '--tab-width', 4 },
+      }),
     }
-
-    if vim.fn.executable('clang-format') == 1 then
-      table.insert(
-        sources,
-        require('null-ls').builtins.formatting.clang_format.with({
-          extra_args = { '-style={IndentWidth: 4}' },
-        })
-      )
-    end
-
-    if vim.fn.executable('black') == 1 then
-      table.insert(sources, require('null-ls').builtins.formatting.black)
-    end
-
-    if vim.fn.executable('prettier') == 1 then
-      table.insert(
-        sources,
-        require('null-ls').builtins.formatting.prettier.with({
-          filetypes = { 'html', 'javascript' },
-          extra_args = { '--tab-width', 4 },
-        })
-      )
-    end
 
     require('null-ls').setup({
       debug = false,

eab586ba4871225bf91d159ea467bed9b3cff33e
Joshua Manchester <joshuamanchester4@gmail.com>
2023-04-25 23:27:43 +0100

    Remove sessions.nvim

--- lua/plugins/workspaces.lua
+++ lua/plugins/workspaces.lua
@@ -2,13 +2,6 @@ return {
   'natecraddock/workspaces.nvim',
   -- Commit before the plugin got unnecessarily bloated
   commit = 'f55ad0a3688840ace255d327a5e5bb16b9ff5a17',
-  dependencies = {
-    'natecraddock/sessions.nvim',
-    opts = {
-      session_filepath = vim.fn.resolve(vim.fn.stdpath('data') .. '/sessions'),
-      absolute = true,
-    },
-  },
   event = 'VeryLazy',
   opts = {
     hooks = {
@@ -16,7 +9,6 @@ return {
         vim.notify('Loaded workspace ' .. require('workspaces').name(), vim.log.levels.INFO, {
           { title = 'Workspaces', timeout = '1000' },
         })
-        require('sessions').load(nil, { silent = true })
       end,
     },
   },

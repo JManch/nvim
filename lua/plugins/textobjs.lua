3f36d837400b01f0abfeb6ab1aab96bb26936180
Joshua Manchester <joshuamanchester4@gmail.com>
2023-05-28 11:02:28 +0100

    Add number textobject

--- lua/plugins/textobjs.lua
+++ lua/plugins/textobjs.lua
@@ -10,13 +10,25 @@ return {
       'iS',
       function() require('various-textobjs').subword(true) end,
       mode = { 'o', 'x' },
-      desc = 'Inner subword text object',
+      desc = 'Textobjs select inner subword',
     },
     {
       'aS',
       function() require('various-textobjs').subword(false) end,
       mode = { 'o', 'x' },
-      desc = 'Around subword text object',
+      desc = 'Textobjs select outer subword',
+    },
+    {
+      'in',
+      function() require('various-textobjs').number(true) end,
+      mode = { 'o', 'x' },
+      desc = 'Textobjs select inner number',
+    },
+    {
+      'an',
+      function() require('various-textobjs').number(false) end,
+      mode = { 'o', 'x' },
+      desc = 'Textobjs select outer number',
     },
   },
 }
